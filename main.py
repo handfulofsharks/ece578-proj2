3ca66d725d776cd2f20c49efce30245fce67de32
Cody <handfulofsharks@gmail.com>
2020-11-11 18:39:51 -0700

    Fixes get_graph_2 to use the new object and adds comment doc strings to functions/classes. Renames node_utils to utils

--- main.py
+++ main.py
@@ -4,40 +4,53 @@ from matplotlib import pyplot as plt
 import sys
 import pandas as pd
 
-from node_utils import ASNode
+from node_utils import *
 
 
 def main(opts):
+    """
+    Main executable function for script.
+
+    Parameters
+    ----------
+    opts : argparse object
+        Object containing variables set by default or the user via command line
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
     check_file_validity([opts.as2_types_file, opts.as_rel2_file])
     as2_type_df = get_df_from_file(opts.as2_types_file)
-    data_dict = ASNode.sort_classifications(as2_type_df)
+    data_dict = sort_classifications(as2_type_df)
     get_graph_1(data_dict)
-    import pdb; pdb.set_trace()
     as_rel2_df = get_df_from_file(opts.as_rel2_file)
-    get_graph_2(df=as_rel2_df.T)
-
-
-def get_df_from_file(file_):
-    column_values_str = None
-    with open(file_, 'r') as f:
-        skip = 0
-        lines = f.readlines()
-    for line in lines:
-        if line.startswith("#"):
-            skip += 1
-        if 'format' in line:
-            column_values_str = lines[0].split(' ')[-1].strip('\n')
-            if column_values_str is not None:
-                continue
-    if column_values_str is None:
-        column_values_str = 'ASa|ASb|Link|Source'
-    sep = list(set([x for x in column_values_str if not x.isalpha() and x != ":" and not x.isnumeric()]))[0]
-    columns_list = column_values_str.split(sep)
-    df = pd.read_csv(file_, header=None, names=columns_list, delimiter=sep, skiprows=skip)
-    return df
+    data_dict = sort_relationships(data_dict, as_rel2_df)
+    get_graph_2(data_dict)
 
 
 def get_graph_1(data_dict):
+    """
+    Given data in the form of a dictionary of AS (Autonomous Systems) Nodes, create a pie chart describing the
+    distribution of the classes of AS Nodes.
+
+    Parameters
+    ----------
+    data_dict : dictionary of AS Node objects
+        data frame that contains AS data in the format of AS | source | Type
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
     labels = ['Transit/Access', 'Content', 'Enterprise']
     bins = [0]*len(labels)
     for i in data_dict:
@@ -52,76 +65,75 @@ def get_graph_1(data_dict):
     ax.axis('equal')
     plt.title(f'AS Categories')
     plt.savefig(f'as_classifications.png', dpi=300, format='png')
-    plt.close()
+    plt.close(fig)
 
 
-def get_graph_2(df):
-    data_dict = {}
-    for i in df:
-        data_dict[df[i].ASa] = ASNode(node_name=df[i].ASa)
-        data_dict[df[i].ASa].connections.append(df[i].ASb)
-        data_dict[df[i].ASa].degree += 1
-        if df[i].ASb in data_dict:
-            data_dict[df[i].ASb].degree += 1
-        else:
-            data_dict[df[i].ASb] = ASNode(node_name=df[i].ASb)
-            data_dict[df[i].ASb].connections.append(df[i].ASb)
-            data_dict[df[i].ASb].degree += 1
-        if df[i].Link == -1:
-            data_dict[df[i].ASa].customer.append(df[i].ASb)
-            
-    bins = [0]*6
+def get_graph_2(data_dict):
+    """
+    Given data in the form of a dictionary of AS (Autonomous Systems) Nodes, create histogram of the node ranking
+    categorized by six bins.
+
+    Parameters
+    ----------
+    data_dict : dictionary of AS Node objects
+        data frame that contains AS data in the format of AS | source | Type
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
+    labels = ['1', '2-5', '6-100', '101-200', '201-1000', '1000+']
+    bins = [0]*len(labels)
     for i in data_dict:
-        import pdb; pdb.set_trace()
-        if data_dict[i]['degree'] == 1:
+        if data_dict[i].degree == 1:
             bins[0] += 1
-        elif 2 < data_dict[i]['degree'] <= 5:
+        elif 2 < data_dict[i].degree <= 5:
             bins[1] += 1
-        elif 5 < data_dict[i]['degree'] <= 100:
+        elif 5 < data_dict[i].degree <= 100:
             bins[2] += 1
-        elif 100 < data_dict[i]['degree'] <= 200:
+        elif 100 < data_dict[i].degree <= 200:
             bins[3] += 1
-        elif 200 < data_dict[i]['degree'] <= 1000:
+        elif 200 < data_dict[i].degree <= 1000:
             bins[4] += 1
-        elif 1000 < data_dict[i]['degree']:
+        elif 1000 < data_dict[i].degree:
             bins[5] += 1
-    bin_names = ['1', '2-5', '6-100', '101-200', '201-1000', '1000+']
-
-    fig1, ax1 = plt.subplots()
-    rects = ax1.bar([0, 1, 2, 3, 4, 5], bins)
-    plt.xticks([0, 1, 2, 3, 4, 5], bin_names)
+    fig, ax1 = plt.subplots()
+    rects = ax1.bar(list(range(len(bins))), bins)
+    for rect in rects:
+        height = rect.get_height()
+        ax1.text(rect.get_x() + rect.get_width() / 2.,
+                 height + 10, '%d' % int(height), ha='center', va='bottom')
+    plt.xticks(list(range(len(labels))), labels)
     plt.title('AS Node Degree Distribution')
     plt.xlabel('Number of Distinct Links')
     plt.ylabel('Number of ASes')
-
-    for rect in rects:
-        height = rect.get_height()
-        ax1.text(rect.get_x() + rect.get_width() / 2., height + 10, '%d' % int(height), ha='center', va='bottom')
-
     plt.savefig('node_degree_dist.png', dpi=300)
-
-
-def check_file_validity(files):
-    validity = 0
-    for file_ in files:
-        if not path.exists(file_) and not path.isfile(file_):
-            print(f'INVALID file: {file_}')
-        else:
-            validity = 1
-            print(f'VALID file:   {file_}')
-    if validity != 1:
-        sys.exit('Could not find user provided files.')
-    else:
-        return
-
-
+    plt.close(fig)
 class Options:
+    """
+    Class to hold values that can be set by the user via command line or maintain default values and then assigns them
+    to a class variable for later use.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
     def __init__(self):
         description = 'Command line inputs for project.'
         parser = argparse.ArgumentParser(description=description)
-        # parses command line inputs.
         inputs = self.parse_args(parser)
-        # assigns inputs from parseArgs function to class members
         self.as2_types_file = inputs.as2_types_file
         self.as_rel2_file = inputs.as_rel2_file
     def parse_args(self, parser):
---
+++ utils.py
@@ -0,0 +1,164 @@
+class ASNode:
+    """
+    Class to contain AS (Autonomous Systems) node information
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
+    def __init__(self, node_name):
+        self.name = node_name
+        self.degree = 0
+        self.connections = list()
+        self.customer = list()
+        self.ip_prefs = list()
+        self.classification = None
+        self.org_type = ''
+        self.org_id = 0
+        self.cone_rank = 0
+        self.ipv4_out = 0
+        self.ipv4_pref_out = 0
+
+
+def sort_classifications(df):
+    """
+    Read in a data frame that contains AS (Autonomous Systems) classification data and parse it into node
+    objects.
+
+    Parameters
+    ----------
+    df : pandas dataframe
+        data frame that contains AS data in the format of AS | source | Type
+
+    Returns
+    -------
+    data_dict
+        dictionary of AS Node objects
+
+    Raises
+    ------
+    None
+    """
+    df = df.set_index(df.columns.values[0])
+    data_dict = dict()
+    for index, row in df.iterrows():
+        data_dict[index] = ASNode(node_name=index)
+        data_dict[index].classification = row.type
+    return data_dict
+
+
+def sort_relationships(data_dict, df):
+    """
+    Read in a data frame that contains AS (Autonomous Systems) relationship data and parse it into node
+    objects.
+
+    Parameters
+    ----------
+    data_dict : dictionary of node objections
+        dictionary of AS Node objects
+    df : pandas dataframe
+        data frame that contains AS data in the format of ASa | ASb | Link
+
+    Returns
+    -------
+    data_dict
+        dictionary of node objects
+
+    Raises
+    ------
+    None
+    """
+    df.set_index(df.columns.values[0], inplace=True)
+    for index, row in df.iterrows():
+        if index in data_dict:
+            data_dict[index].degree += 1
+            data_dict[index].connections.append(row.ASb)
+        else:
+            data_dict[index] = ASNode(node_name=index)
+            data_dict[index].degree += 1
+            data_dict[index].connections.append(row.ASb)
+        if row.ASb in data_dict:
+            data_dict[index].degree += 1
+            data_dict[index].connections.append(index)
+        else:
+            data_dict[row.ASb] = ASNode(node_name=row.ASb)
+            data_dict[row.ASb].degree += 1
+            data_dict[row.ASb].connections.append(index)
+        if row.Link == -1:
+            data_dict[index].customer.append(row.ASb)
+    return data_dict
+
+
+def check_file_validity(files):
+    """
+    Given a list of files, verifies that files can be found.
+
+    Parameters
+    ----------
+    files : list of strings
+        list containing file path strings
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    None
+    """
+    validity = 0
+    for file_ in files:
+        if not path.exists(file_) and not path.isfile(file_):
+            print(f'INVALID file: {file_}')
+        else:
+            validity = 1
+            print(f'VALID file:   {file_}')
+    if validity != 1:
+        sys.exit('Could not find user provided files.')
+    else:
+        return
+
+
+def get_df_from_file(file_):
+    """
+    Given a file, reads in that file via pandas and attempts to correctly determine header information
+
+    Parameters
+    ----------
+    file_ : string
+        String of the path to the file
+
+    Returns
+    -------
+    df: pandas dataframe
+        dataframe containing information from the file read
+
+    Raises
+    ------
+    None
+    """
+    column_values_str = None
+    with open(file_, 'r') as f:
+        skip = 0
+        lines = f.readlines()
+    for line in lines:
+        if line.startswith("#"):
+            skip += 1
+        if 'format' in line:
+            column_values_str = lines[0].split(' ')[-1].strip('\n')
+            if column_values_str is not None:
+                continue
+    if column_values_str is None:
+        column_values_str = 'ASa|ASb|Link|Source'
+    sep = list(set([x for x in column_values_str if not x.isalpha() and x != ":" and not x.isnumeric()]))[0]
+    columns_list = column_values_str.split(sep)
+    df = pd.read_csv(file_, header=None, names=columns_list, delimiter=sep, skiprows=skip)
+    return df
